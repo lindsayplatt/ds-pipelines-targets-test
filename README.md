08351ac194d0e0a475ad6e33a3b3b8e0cb4ce871
Platt <lplatt@usgs.gov>
2021-03-09 16:45:14 -0600

    pipelines 3 with only 3 states at first

--- README.md
+++ README.md
@@ -5,3 +5,28 @@ To follow along, I couldn't do the actual course in Git Learning Lab because I w
 * [ds-pipelines-1 steps](https://github.com/USGS-R/ds-pipelines-1-course/tree/master/responses)
 * [ds-pipelines-2 steps](https://github.com/USGS-R/ds-pipelines-2-course/tree/master/responses)
 * [ds-pipelines-3 steps](https://github.com/USGS-R/ds-pipelines-3-course/tree/master/responses)
+
+# How I setup pipelines-3
+
+I had already completed [the actual GitHub Learning Lab course](https://lab.github.com/USGS-R/scipiper-tips-and-tricks), so I went to [my course repo](https://github.com/lindsayplatt/ds-pipelines-3) and copied the `scipiper` pipeline code needed into this repo. I copied all of the `src/` files, the `remake.yml`, and the `123_state_tasks.R` file. Then, I was ready to convert the `scipiper` pipeline into a `targets` pipeline. When finished, I deleted `remake.yml` and `123_state_tasks.R` because they were not needed.
+
+# Build pipelines-3
+
+I wrote code that recreates the pipeline from ds-pipelines-3 using `targets`. I didn't not recreate EXACTLY because I took some liberties when it came to translating the task table into a `targets` `tar_map` step for what would make the most sense. Overall, I found that the `targets` code is much more concise than the scipiper pipeline code. I kept all targets in the `_targets.R` file this time around.
+
+There are 6 top-level targets (`oldest_active_sites`, `combine_obs_tallies`, `state_timeseries_files`, `plot_all_data`, `map_all_data`, and `map_interactive_timeseries`. In the `tar_map` command, there are 4 targets per state (`state_info`, `state_data`, `state_timeseries_plot`, and `state_tally`). In the end (ignoring the files saved in `1_fetch/tmp` and `3_visualize/tmp`), three files should build: `3_visualize/out/data_coverage.png`, `3_visualize/out/site_map.png`, and `3_visualize/out/timeseries_map.html`. Note that all of the directories need to be setup ahead of time; `tar_make()` will not create an `*/out/` or `*/tmp/` folders; it will throw an error if you don't have the correct directories.
+
+Before running this pipeline, I tested that it was ready by changing `states` to only two states and then running `tar_glimpse()` to investigate that all of the connections are appropriately setup. I get the following:
+
+![image of all the steps in my targets pipeline linked for two states, WI and VA](img/targets_pipeline_ready.png)
+
+Then, I built the pipeline by running the code below:
+
+```r
+library(targets)
+tar_make()
+```
+
+Once it built, I added `NY` to `states` and checked what would update by running `tar_visnetwork()`. I got the following:
+
+![image of what steps in my targets pipeline need to rebuild after the addition of NY](img/targets_pipeline_rebuild_with_NY.png)
---
+++ _targets.R
@@ -0,0 +1,75 @@
+library(targets)
+library(tarchetypes)
+library(tibble) # When used in `tar_map`, this seems to need to be loaded explicitly
+
+options(tidyverse.quiet = TRUE)
+tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr", "rnaturalearth", 
+                            "cowplot", "leaflet", "leafpop", "htmlwidgets", "lubridate"))
+
+# Load functions needed at top level of pipeline
+source("1_fetch/src/find_oldest_sites.R")
+source("1_fetch/src/get_site_data.R")
+source("2_process/src/tally_site_obs.R")
+source("3_visualize/src/plot_site_data.R")
+source("3_visualize/src/plot_data_coverage.R")
+source("3_visualize/src/map_sites.R")
+source("3_visualize/src/map_timeseries.R")
+
+# Some basic config:
+states <- c("WI", "VA", "NY")
+# states <- c('AL','AZ','AR','CA','CO','CT','DE','DC','FL','GA','ID','IL','IN','IA',
+#             'KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
+#             'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX',
+#             'UT','VT','VA','WA','WV','WI','WY','AK','HI','GU','PR')
+
+parameter <- '00060'
+
+# First, identify sites
+t1 <- tar_target(oldest_active_sites, find_oldest_sites(states, parameter))
+
+# Then do tasks by state
+targets_by_state <- tar_map(
+  values = tibble(state_abbr = states, 
+                  state_info_file = sprintf("1_fetch/tmp/inventory_%s.tsv", state_abbr),
+                  plot_file = sprintf("3_visualize/tmp/timeseries_%s.png", state_abbr)),
+  names = state_abbr,
+  tar_target(state_info, filter(oldest_active_sites, state_cd == state_abbr)),
+  tar_target(state_data, get_site_data(state_info, parameter)),
+  tar_target(state_timeseries_plot, plot_site_data(plot_file, state_data, parameter), format = "file"),
+  tar_target(state_tally, tally_site_obs(state_data))
+)
+
+# Now that mapping is complete, put all remaining targets into single list
+# which is a `targets` requirement
+list(
+  t1,
+  targets_by_state,
+  
+  # Combine obs into one obs tibble
+  tar_combine(
+    combine_obs_tallies,
+    targets_by_state$state_tally,
+    command = dplyr::bind_rows(!!!.x, .id = "state_cd")
+  ),
+  
+  # Combine state plots into single vector of filepaths (default of tar_combine creates a vector)
+  tar_combine(state_timeseries_files, targets_by_state$state_timeseries_plot),
+  
+  # Using all state data make a plot of data coverage, a map of all sites, and an interactive HTML
+  # to explore data by state
+  tar_target(
+    plot_all_data,
+    plot_data_coverage(combine_obs_tallies, "3_visualize/out/data_coverage.png", parameter),
+    format = "file"
+  ),
+  tar_target(
+    map_all_data,
+    map_sites(oldest_active_sites, "3_visualize/out/site_map.png"),
+    format = "file"
+  ),
+  tar_target(
+    map_timeseries_interactive,
+    map_timeseries(oldest_active_sites, state_timeseries_files, "3_visualize/out/timeseries_map.html"),
+    format = "file"
+  )
+)
